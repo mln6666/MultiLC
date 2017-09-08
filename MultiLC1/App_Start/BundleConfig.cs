9b611c2fbfe0b3ba853d80b1d1745e97dfe19a1a
Mario Leonardo Ponce <ponce_marioleo@hotmail.com>
2017-09-07 22:34:17 -0300

    + Bundles +JS bases

--- MultiLC1/App_Start/BundleConfig.cs
+++ MultiLC1/App_Start/BundleConfig.cs
@@ -8,24 +8,38 @@ namespace MultiLC1
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = true;
+            ScriptBundle scriptBundle = new ScriptBundle("~/js");
+            string[] scriptArray =
+            {
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery-3.1.1.min.js",
+                "~/Scripts/toastr.js",
+                "~/Scripts/DataTables/jquery.datatables.min.js",
+                "~/Scripts/DataTables/datatables.uikit.min.js",
+                "~/Scripts/jquery.unobtrusive-ajax.min.js"
+            };
+            scriptBundle.Include(scriptArray);
+            scriptBundle.IncludeDirectory("~/Scripts/", "*.js");
+            bundles.Add(scriptBundle);
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Styles/css").Include(
+                "~/Content/Dashboard/css/bootstrap.min.css",
+                "~/Content/toastr.css",
+                "~/Content/DataTables/css/uikit.min.css",
+                "~/Content/DataTables/css/dataTables.uikit.min.css"
+
+            ));
         }
     }
 }
